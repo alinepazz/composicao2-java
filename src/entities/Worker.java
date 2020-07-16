3e2daafa00ac25a0af4dd71eaec4f9ec6fa7f35f
aline <aline.soares.2@hotmail.com>
2020-07-16 19:40:09 -0300

    trabalhando com lista

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.enums.WorkerLevel;
 
 public class Worker {
@@ -7,4 +10,5 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
+	private List<HourContract> contracts = new ArrayList<>();
 }
