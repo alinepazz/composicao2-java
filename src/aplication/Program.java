fa7a11d2bc27ac499e81745ca1cd9697859381d7
aline <aline.soares.2@hotmail.com>
2020-07-17 18:54:49 -0300

    informações iniciais

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,7 +1,12 @@
 package aplication;
 
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkerLevel;
 
 public class Program {
 
@@ -10,7 +15,27 @@ public class Program {
 		
 		Scanner c = new Scanner(System.in);
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		System.out.print("Enter department's name:");
+		String department = c.nextLine();
+		Department depart = new Department(department);
+		
+		System.out.println("Enter worker data:");
+		
+		System.out.print("Name: ");
+		String name = c.nextLine();
+		
+		System.out.print("Level: ");
+		WorkerLevel level = WorkerLevel.valueOf(c.nextLine());
+		
+		System.out.print("Base salary:");
+		double salary = c.nextDouble();
+		
+		Worker worker = new Worker(name, level, salary);
+		
+		
+
 		c.close();
 
 	}
