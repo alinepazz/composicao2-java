421b95e01045894ee8bbbe7a5ff1327344c084e8
aline <aline.soares.2@hotmail.com>
2020-07-17 19:21:02 -0300

    ajustando o Program

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,16 +1,19 @@
 package aplication;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		
 		Scanner c = new Scanner(System.in);
@@ -34,7 +37,26 @@ public class Program {
 		
 		Worker worker = new Worker(name, level, salary);
 		
+		System.out.println("How many contracts to this worker?");
+		int contract = c.nextInt();
 		
+		for(int i=1; i <=contract; i++) {
+			System.out.println("Enter contract #"+ i + "data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date date = sdf.parse(c.next());
+			
+			System.out.print("Value per hour:");
+			double valuePerHour = c.nextDouble();
+			
+			System.out.print("Duration (hours): ");
+			int hora = c.nextInt();
+			
+			HourContract contracts = new HourContract(date, valuePerHour, hora);
+			worker.addContract(contracts);
+			
+			System.out.print("Enter month and year to calculate income (MM/YYYY):");
+			
+		}
 
 		c.close();
 
